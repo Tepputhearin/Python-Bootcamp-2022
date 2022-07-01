b4326312ebf63fc828583cc4f715c5a1bcfab119
Tepputhearin <tepputhearin21@kit.edu.kh>
2022-07-01 10:23:39 +0700

    Illegal

--- week01/ex/20_encode_decode.py
+++ week01/ex/20_encode_decode.py
@@ -1,12 +1,23 @@
-list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z']
-mid = len(list)//2+1
-list1 = list[:mid]
-list2 = list[mid:]
 def encode():
-    list1 = list[:mid]
-    list2 = list[mid:]
-    join1 = "".join(list1)
-    join2 = "".join(list2)
-    
-
-encode()
+    userstr = input("Enter the string to encode:\n")
+    dict = {'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S', 'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z','N':'A', 'O':'B', 'P':'Q', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I', 'W':'J', 'X':'K', 'Y':'L', 'Z':'M'}
+    list = []
+    for i in userstr:
+        convert = list.append(dict[i])
+    print("The endcode text is:","".join(list))
+def decode():
+    userstr = input("Enter the string to decode:\n")
+    dict = {'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q', 'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U', 'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y', 'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'Q', 'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G', 'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K', 'Y': 'L', 'Z': 'M'}
+    list = []
+    for i in userstr:
+        convert = list.append(dict[i])
+    print("The decoded text is:","".join(list))
+while True:
+    userch = input("Press 1 to encode\nPress 2 to decode\n")
+    if userch == "1":
+        encode()
+    elif userch == "2":
+        decode()
+    usercon = input("Do you want to continue?[Y/N]\n")
+    if usercon == "N":
+        break
