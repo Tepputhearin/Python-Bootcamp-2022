b02056410722af2d6050067c6451c5e910272193
Tepputhearin <tepputhearin21@kit.edu.kh>
2022-07-29 08:08:27 +0700

    OH

--- week03/ex/42_dec_to_oct.py
+++ week03/ex/42_dec_to_oct.py
@@ -4,8 +4,8 @@ def dec_to_oct(num):
         octal_num = num % 8
         num = int(num/8)
         oct.append(octal_num)
-    for j in range(len(oct)- 1, -1, -1):
+    for j in range(len(oct) - 1, -1, -1):
         print(oct[j], end="")
     return
 
-dec_to_oct(697)
+dec_to_oct(98)
